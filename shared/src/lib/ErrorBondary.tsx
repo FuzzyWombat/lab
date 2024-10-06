6a1fc446c335a79ded4589898bc6064d00371b7b
Ryan Gibson <RyanGibson365@gmail.com>
2024-10-06 15:06:48 -0500

    Fix Error Boundary

--- shared/src/lib/ErrorBondary.tsx
+++ shared/src/lib/ErrorBondary.tsx
@@ -1,25 +1,30 @@
 import React from 'react'
 
-export class ErrorBoundary extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = { hasError: false };
-    }
+type ErrorBoundaryProps = {
+    children: React.ReactNode;
+    fallback?: React.ReactNode;
+}
+
+type State = {
+    hasError?: boolean;
+}
+
+export class ErrorBoundary extends React.Component<ErrorBoundaryProps> {
+    public state: State = {hasError: false}
   
-    static getDerivedStateFromError(error) {
+    static getDerivedStateFromError(_: Error): State {
       // Update state so the next render will show the fallback UI.
       return { hasError: true };
     }
   
-    componentDidCatch(error, errorInfo) {
+    componentDidCatch(error: Error, errorInfo: React.ErrorInfo): void {
       // You can also log the error to an error reporting service
-      console.log(error)
+      console.error('Error Boundary Error: ',error,errorInfo)
     }
   
     render() {
       if (this.state.hasError) {
-        // You can render any custom fallback UI
-        return <h1>Something went wrong.</h1>;
+        return this.props.fallback ? this.props.fallback : <section>Something Went Wrong. Please Try Again!</section>
       }
   
       return this.props.children; 
